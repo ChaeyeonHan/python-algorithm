4bce33cb9a0fb51222b53fdebcc4287b44f88c06
ChaeyeonHan <kate2236e@naver.com>
2021-11-25 23:51:56 +0900

    #1712

---
+++ 1712-손익분기점.py
@@ -0,0 +1,9 @@
+fcost, mcost, sell_cost = map(int, input().split())
+# split으로 입력받은 값을 공백을 기준으로 분리
+# map과 함께 사용하여 split의 결과를 모두 int로 변환
+# 판매갯수 x개.
+
+if (mcost >= sell_cost):
+    print(-1) # 손익분기점 존재하지 않음
+else:
+    print(fcost//(sell_cost-mcost)+1)
