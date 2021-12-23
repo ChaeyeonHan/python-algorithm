1d19f1a4838afc930cd544b99c15c42381611dd9
ChaeyeonHan <kate2236e@naver.com>
2021-12-23 13:33:17 +0900

    모험가 길드

---
+++ 그리디/모험가 길드.py
@@ -0,0 +1,13 @@
+N = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+count = 0
+result = 0
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
