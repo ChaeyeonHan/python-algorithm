fa5524327e1de1a1eee072ce75716f3cef6e9fc0
ChaeyeonHan <kate2236e@naver.com>
2022-01-01 20:16:57 +0900

    모험가 길드

--- 그리디/모험가 길드.py
+++ 그리디/모험가 길드.py
@@ -2,12 +2,12 @@ N = int(input())
 data = list(map(int, input().split()))
 data.sort()
 
-count = 0
-result = 0
+count = 0  # 현재 그룹의 모험가 카운트
+result = 0  # 가능한 그룹수 카운트
 for i in data:
     count += 1
     if count >= i:
         result += 1
-        count = 0
+        count = 0  # 모험가 수 초기화
 
 print(result)
