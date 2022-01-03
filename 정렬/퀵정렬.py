5548e3caaa8565616d6d5fbfc1874b1ba6721db0
ChaeyeonHan <kate2236e@naver.com>
2022-01-03 19:40:32 +0900

    퀵정렬

--- 정렬/퀵정렬.py
+++ 정렬/퀵정렬.py
@@ -29,3 +29,21 @@ def quick_sort(array, start, end):
 
 quick_sort(array, 0, len(array)-1)
 print(array)
+
+# 파이썬 장점을 살린 퀵 정렬
+array = [5, 7 ,9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    if len(array) <= 1:  # 리스트가 하나 이하의 원소를 갖고있다면 종료
+        return array
+
+    pivot = array[0]
+    tail = array[1:]  # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot]  # 왼쪽부분, 피벗보다 큰거 찾기
+    right_side = [x for x in tail if x > pivot]  # 오른쪽부분, 피벗보다 작은거 찾기
+
+    # 왼쪽과 오른쪽 부분에서 각각 퀵정렬을 수행후, 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
