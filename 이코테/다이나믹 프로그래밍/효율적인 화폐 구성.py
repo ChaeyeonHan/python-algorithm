49a051af59b5a5e9ac8f3f8fec17171139918a17
ChaeyeonHan <kate2236e@naver.com>
2022-08-06 01:13:19 +0900

    효율적인 화폐 구성

--- 이코테/다이나믹 프로그래밍/효율적인 화폐 구성.py
+++ 이코테/다이나믹 프로그래밍/효율적인 화폐 구성.py
@@ -4,3 +4,15 @@ coins=[]
 for i in range(n):
     coins.append(int(input()))
 
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(coins[i], m+1):
+        if (d[j - coins[i]]) != 10001:
+            d[j] = min(d[j], d[j-coins[i]] + 1)
+
+if d[n] == 10001:
+    print(-1)  # m원을 만들지 못하는 경우
+else:
+    print(d[m])
