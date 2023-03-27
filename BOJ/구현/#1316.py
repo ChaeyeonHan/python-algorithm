503fa850ff84f955d608b67b8396e52697ae10b6
ChaeyeonHan <kate2236e@naver.com>
2023-03-27 23:53:12 +0900

    #1316

--- BOJ/구현/#1316.py
+++ BOJ/구현/#1316.py
@@ -9,3 +9,12 @@ for i in range(n):
                 cnt -= 1
                 break
 print(cnt)
+
+# for i in range(n):
+#     word = input()
+#     for j in range(0, len(word)-1):
+#         if word[j] == word[j+1]:
+#             pass
+#         elif word[j] in word[j+1:]:
+#             cnt -= 1
+#             break
