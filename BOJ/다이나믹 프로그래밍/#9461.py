59655ea927a3e9d02c9d9a11dda9a94e289de67f
ChaeyeonHan <kate2236e@naver.com>
2023-06-05 15:09:35 +0900

    #9461

--- BOJ/다이나믹 프로그래밍/#9461.py
+++ BOJ/다이나믹 프로그래밍/#9461.py
@@ -13,3 +13,23 @@ for i in range(3, 101):
 for _ in range(T):
     N = int(input())
     print(P[N])
+
+# 규칙 찾아서
+T = int(input())
+P = [0]* 101
+P[1] = 1
+P[2] = 1
+P[3] = 1
+P[4] = 2
+P[5] = 2
+P[6] = 3
+P[7] = 4
+P[8] = 5
+P[9] = 7
+
+for i in range(10, 101):
+    P[i] = P[i-1] + P[i-5]
+
+for _ in range(T):
+    N = int(input())
+    print(P[N])
