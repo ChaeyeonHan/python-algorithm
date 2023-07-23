1a7201c5daba0cebcc05037a24993cb46d7c6d80
ChaeyeonHan <kate2236e@naver.com>
2023-07-23 19:52:47 +0900

    #11724

--- BOJ/DFS, BFS/#11724.py
+++ BOJ/DFS, BFS/#11724.py
@@ -31,3 +31,31 @@ for i in range(1, n+1):
         bfs(graph, i, visited)
         count += 1
 print(count)
+
+# DFS 풀이
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 재귀
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+cnt = 0
+for i in range(1, N+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        cnt += 1
+print(cnt)
